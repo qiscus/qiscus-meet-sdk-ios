a0a55f58feac6b31cf9aa2ecd533df26e4c49ff5
rajapulau <rajapulau@gmail.com>
2020-01-25 17:25:46 +0700

    update to xcode 10.3.1

--- QiscusMeetExample/QiscusMeetExample/ViewController.swift
+++ QiscusMeetExample/QiscusMeetExample/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
     }
     
     func call(isVideo: Bool, roomName : String){
-        let vc = QiscusMeet.answer(isVideo: isVideo, room: roomName, avatarUrl: "https://filmschoolrejects.com/wp-content/uploads/2017/04/0JRofTsuy93evl_J5.jpg", displayName: "arief", onSuccess: { (vc) in
+        let vc = QiscusMeet.call(isVideo: isVideo, room: roomName, avatarUrl: "https://filmschoolrejects.com/wp-content/uploads/2017/04/0JRofTsuy93evl_J5.jpg", displayName: "arief", onSuccess: { (vc) in
             self.navigationController?.present(vc, animated: true, completion: {
                 
             })
