06b15df7dc567273f9d9bd6c307de2cddcf4c504
ariefnurputranto <arief.themil@gmail.com>
2020-03-30 13:17:30 +0700

    change baseServer url

--- QiscusMeetExample/QiscusMeetExample/AppDelegate.swift
+++ QiscusMeetExample/QiscusMeetExample/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        QiscusMeet.setup(url: "https://qlue-call.qiscus.com")
+        QiscusMeet.setup(url: "https://meet.qiscus.com")
         return true
     }
 
