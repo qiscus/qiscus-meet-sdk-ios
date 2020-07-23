befcfaf81b4082670dca81253bc399fc226a2f06
rajapulau <rajapulau@gmail.com>
2020-07-23 21:46:10 +0700

    change base url

--- QiscusMeetExample/QiscusMeetExample/AppDelegate.swift
+++ QiscusMeetExample/QiscusMeetExample/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Thread.sleep(forTimeInterval: 3.0)
-        QiscusMeet.setup(url: "https://meetstage.qiscus.com")
+        QiscusMeet.setup(url: "https://meet.qiscus.com")
         
         FirebaseApp.configure()
         
