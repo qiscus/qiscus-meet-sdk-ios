2c05a9c304163d647567b32e3f69eb689e66bfd8
rajapulau <rajapulau@gmail.com>
2021-01-19 12:58:38 +0700

    fix build catalina

---
+++ QiscusMeetExample/Pods/QiscusMeet/QiscusMeet/QiscusMeetConfig.swift
@@ -0,0 +1,20 @@
+//
+//  QiscusMeetConfig.swift
+//  FirebaseCore
+//
+//  Created by Ganjar Widiatmansyah on 14/08/20.
+//
+
+import Foundation
+
+public class QiscusMeetConfig: NSObject {
+    public static var shared = QiscusMeetConfig()
+    @objc public var setPassword = false
+    @objc public var setChat = false
+    @objc public var setOverflowMenu = false
+    @objc public var setVideoThumbnailsOn = true
+    @objc public var setJwtConfig = MeetJwtConfig()
+    @objc public var setEnableRoomName = true
+    
+    fileprivate override init(){}
+}
